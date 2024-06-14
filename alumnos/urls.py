652755da89fab43b3e73f9631ce71df382461ecf
NaTheFunny <matiasbetorr@gmail.com>
2024-06-14 10:23:44 -0400

    Tercera Carga

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('index',views.index, name='index'),
     path('crud',views.crud, name='crud'),
     path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
+    path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),
+    path('alumnos_findEdit/<str:pk>', views.alumnos_findEdit, name='alumnos_findEdit'),
+    path('alumnosUpdate', views.alumnosUpdate, name='alumnosUpdate'),
 ]
